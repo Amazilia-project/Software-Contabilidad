672f3b932d1b7f8320420d09a8b5db7a20b8d705
Kevin Herrera <k3v1n3manu37@gmail.com>
2020-09-12 23:45:14 -0600

    Centralizacion de la ventana login al abrir y creacion de una ventana de dialogo para el boton de informacion

--- login.py
+++ login.py
@@ -13,34 +13,37 @@ class Login:
 		self.pas.delete(0, 'end')
 		self.name.focus()
 
-	def configurar(self):
-		dialogo1 = nueva_ventana(self.window)
-		s=dialogo1.mostrar()
 
-	def nueva_ventana(self):
-		self.infor_wind = Toplevel()
-		self.infor_wind.title = 'Editar Producto'
+	def informacion(self):
+		self.infor_wind = Toplevel()	# Popup = Toplevel()
+		self.infor_wind.title("Información Relevante")
+
+		ttk.Button(self.infor_wind, text = "Salir", command = self.infor_wind.destroy).pack(padx=10, pady=10)
+		
+		self.infor_wind.transient(self.wind)	# Desabilitar el boton de minimizar infor_wind  
+		self.infor_wind.grab_set()	# La interacción de la ventana principal "wind" es deshabilitada 
+		self.wind.wait_window(self.infor_wind)	# Detener la ventana principal wind
 
 
 	def __init__(self, window):
-		# Funcion principal para inicializar y centrar ventana
+		# Funcion principal para inicializar
 		self.wind = window
 		self.wind.title("Amazilia Python")
 		self.wind.resizable(0, 0)
 		#self.wind.geometry ("390x264")
-		#self.wind.iconbitmap('./image/hola.ico')
+		#self.wind.iconbitmap('./image/.ico')
 
+		# Centrar ventana
 		window_width = 390
 		window_height = 264
 
-		screen_width = self.wind.winfo_screenwidth()
-		screen_height = self.wind.winfo_screenheight()
+		screen_width = self.wind.winfo_screenwidth()	# winfo() es para obtener informacion de la ventana actual
+		screen_height = self.wind.winfo_screenheight()	
 
-		x_cordinate = int((screen_width/2) - (window_width/2))
-		y_cordinate = int((screen_height/2) - (window_height/2))
+		x_coordenada = int((screen_width/2) - (window_width/2))
+		y_coordenada = int((screen_height/2) - (window_height/2))
 
-		self.wind.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
-
+		self.wind.geometry("{}x{}+{}+{}".format(window_width, window_height, x_coordenada, y_coordenada))
 
 
 		# Contenedor 1 (Frame 1) de marcos
@@ -60,6 +63,7 @@ class Login:
 		self.pas.config(show = "*")
 
 
+		# Estilo de botones
 		style = ttk.Style() 
 		style.configure('TButton', font = ("Helvetica", 12))
 		#style.configure('TButton', width=10) # Tamaño del boton
@@ -74,16 +78,13 @@ class Login:
 		frame2.grid(row = 4, column = 0, columnspan = 3, padx = 15, ipady = 3)
 
 		# Botones de operaciones
-		ttk.Button(frame2, text = "Información",style = 'TButton', command = self.nueva_ventana).grid(row = 5, column = 1, pady=5, padx = 5)
+		ttk.Button(frame2, text = "Información",style = 'TButton', command = self.informacion).grid(row = 5, column = 1, pady=5, padx = 5)
 		ttk.Button(frame2, text = "Reset",style = 'TButton', command = self.limpiar_text).grid(row = 5, column = 2, pady = 5, padx = 5)
 		ttk.Button(frame2, text = "Exit",style = 'TButton', command = self.wind.quit).grid(row = 5, column = 3, pady = 5, padx = 5)
 
 		# Espacio en blanco
 		self.espacio = Label(text = "")
 		self.espacio.grid(row = 6, column = 0, columnspan = 2, sticky = W + E)
-
-
-
 
 
 
